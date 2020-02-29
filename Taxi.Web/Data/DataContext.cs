ee3e1658190061aa7fd68aea55c97405102cba6c
JS <trasc@DESKTOP-P3M7BJP>
2020-02-29 15:52:31 -0600

    CRUD de Taxi

---
+++ Taxi.Web/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿namespace Taxi.Web.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using Taxi.Web.Data.Entities;
+
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options)
+        {
+            
+        }
+        public DbSet<TaxiEntity> Taxis { get; set; }
+    }
+}
---
+++ Taxi.Web/Data/Entities/TaxiEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace Taxi.Web.Data.Entities
+{
+    using System.ComponentModel.DataAnnotations;
+    public class TaxiEntity
+    { 
+        public int Id { get; set; }
+        [RegularExpression(@"^([A-Za-z]{3}\d{3})$", ErrorMessage = "The field {0} must starts with three characters and ends with numbers.")]
+        [StringLength(6, MinimumLength = 6, ErrorMessage = "The {0} field must have {1} characters.")]
+        [Required(ErrorMessage ="The field {0} is mandatory.")]
+        public string Plaque { get; set; }
+    }
+}
